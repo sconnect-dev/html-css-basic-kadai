1eabceb1ef549a2df1df101955c4c83c27a7fac8
sconnect-dev <s_connect@icloud.com>
2025-07-04 04:45:42 +0900

    関数をMDNで調べて使ってみよう

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const date = new Date(); // 月は0ベースなので10月は9を指定
+
+const y = date.getFullYear();
+const m = date.getMonth() +1; // 月を1ベースに変換
+const d = date.getDate(); // 日付を取得
+
+console.log(`${y}年${m}月${d}日`);
