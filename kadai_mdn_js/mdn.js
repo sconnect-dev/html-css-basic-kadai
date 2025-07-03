960d5c12e8221e18c53ae46dbe26ce4801c20ffe
sconnect-dev <s_connect@icloud.com>
2025-07-04 04:43:23 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-const date = new Date(); // 月は0ベースなので10月は9を指定
-
-const y = date.getFullYear()-1;
-const m = date.getMonth() +4; // 月を1ベースに変換
-const d = date.getDate()+8; // 日付を取得
-
-console.log(`${y}年${m}月${d}日`);
